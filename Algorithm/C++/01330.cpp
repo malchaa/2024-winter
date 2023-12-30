869352cbd3dc771d086b87243318aa67c24d3ffe
malchaa <malchaadev@gmail.com>
2023-12-30 19:19:47 +0900

    231230

---
+++ Algorithm/C++/01330.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a,b;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> a >> b;
+    if(a>b) cout << ">";
+    else if(a<b) cout << "<";
+    else cout << "==";
+    return 0;
+}
