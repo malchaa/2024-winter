3fe04dd5170d8efbfd7459d28a7d3c29987208b8
malchaa <malchaadev@gmail.com>
2023-12-31 12:59:31 +0900

    231231

---
+++ Algorithm/C++/14681.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int x, y;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> x >> y;
+    if(x > 0 && y > 0) cout << "1";
+    else if (x < 0 && y > 0) cout << "2";
+    else if (x < 0 && y < 0) cout << "3";
+    else if (x > 0 && y < 0) cout << "4";
+    return 0;
+}
