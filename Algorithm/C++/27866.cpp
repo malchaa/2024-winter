60ff1b93b2a61cc5077c522926d631f2d8e8cbee
malchaa <malchaadev@gmail.com>
2024-01-07 21:33:31 +0900

    240107

---
+++ Algorithm/C++/27866.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int index;
+    string s;
+    cin >> s >> index;
+    cout<< s[index - 1];
+    return 0;
+}
