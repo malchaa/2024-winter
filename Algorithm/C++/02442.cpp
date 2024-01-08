fa68770c4ff570bc2b4cbc77373b52aaff43fd53
malchaa <malchaadev@gmail.com>
2024-01-08 15:16:55 +0900

    240108

---
+++ Algorithm/C++/02442.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N - i; j++) cout << ' ';
+        for(int j = 1; j <= 2 * i - 1; j++) cout << '*';
+        // '*'의 오른쪽 공백은 출력하지 말아야 통과.
+        cout << "\n";
+    }
+}
